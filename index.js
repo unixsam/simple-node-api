41350263c839eb60e651eda770152a97b8319f04
Sami Ahmed <unixsam@gmail.com>
2023-03-25 01:57:27 +0400

    Add post router path

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const app = express();
+
+app.use(express.json());
+
 const port = process.env.PORT || 3000;
 
 app.get('/api', (req, res) => {
   res.json({ message: 'Hello from the Simple Node.js API!' });
 });
+
+app.post('/api/data', (req, res) => {
+    const data = req.body;
+    console.log('Received JSON object:', data);
+    res.json({ message: 'JSON object received successfully!', data });
+});
+  
 
 app.listen(port, () => {
   console.log(`API is running on port ${port}`);

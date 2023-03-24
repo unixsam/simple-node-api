3ee088f84e9d601290112f500e359acf5738c748
Sami Ahmed <unixsam@gmail.com>
2023-03-25 02:01:15 +0400

    update post function

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
-
-app.use(express.json());
-
 const port = process.env.PORT || 3000;
+
+// Use body-parser middleware to parse JSON data
+app.use(bodyParser.json());
 
 app.get('/api', (req, res) => {
   res.json({ message: 'Hello from the Simple Node.js API!' });
 });
 
-app.post('/api/data', (req, res) => {
-    const data = req.body;
-    console.log('Received JSON object:', data);
-    res.json({ message: 'JSON object received successfully!', data });
+// Add a POST route to handle incoming JSON objects
+app.post('/api', (req, res) => {
+  const receivedData = req.body;
+  console.log('Received JSON object:', receivedData);
+  res.status(200).json({ message: 'JSON object received', data: receivedData });
 });
-  
 
 app.listen(port, () => {
   console.log(`API is running on port ${port}`);

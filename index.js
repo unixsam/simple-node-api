baae44ba2f66315b74215730b53340ef5addd54b
Sami Ahmed <unixsam@gmail.com>
2023-03-25 13:32:34 +0400

    finalizing index.js

--- index.js
+++ index.js
@@ -21,41 +21,39 @@ connection.connect((err) => {
 });
 
 app.get('/api', (req, res) => {
-  res.status(200).json({ message: 'JSON object received and data inserted into MySQL', data: req.body });
+  res.json({ message: 'Hello from the Simple Node.js API!' });
 });
 
 // Add a new GET route to test MySQL connection
 app.get('/api/test-connection', (req, res) => {
-    connection.ping((err) => {
-      if (err) {
-        console.error('Error testing MySQL connection:', err.stack);
-        res.status(500).json({ message: 'Error testing MySQL connection', error: err });
-      } else {
-        res.status(200).json({ message: 'MySQL connection is successful!' });
-      }
-    });
+  connection.ping((err) => {
+    if (err) {
+      console.error('Error testing MySQL connection:', err.stack);
+      res.status(500).json({ message: 'Error testing MySQL connection', error: err });
+    } else {
+      res.status(200).json({ message: 'MySQL connection is successful!' });
+    }
   });
-  
+});
 
 // Add a POST route to handle incoming JSON objects
 app.post('/api', (req, res) => {
   const receivedData = req.body;
   console.log('Received JSON object:', receivedData);
-  res.json({data: req});
+
   // Insert data into the MySQL database
   const query = 'INSERT INTO sensor_data (dateTime, temperature, humidity, relay_status) VALUES (?, ?, ?, ?)';
   const values = [receivedData.dateTime, receivedData.temperature, receivedData.humidity, receivedData.relay_status];
 
-  //res.status(200).json({ message: 'submitted data', data: receivedData });
-//   connection.query(query, values, (err, result) => {
-//     if (err) {
-//       console.error('Error inserting data into MySQL:', err.stack);
-//       res.status(500).json({ message: 'Error inserting data into MySQL', error: err });
-//     } else {
-//       console.log('Data inserted into MySQL with ID:', result.insertId);
-//       res.status(200).json({ message: 'JSON object received and data inserted into MySQL', data: receivedData });
-//     }
-//   });
+  connection.query(query, values, (err, result) => {
+    if (err) {
+      console.error('Error inserting data into MySQL:', err.stack);
+      res.status(500).json({ message: 'Error inserting data into MySQL', error: err });
+    } else {
+      console.log('Data inserted into MySQL with ID:', result.insertId);
+      res.status(200).json({ message: 'JSON object received and data inserted into MySQL', data: receivedData });
+    }
+  });
 });
 
 app.listen(port, () => {

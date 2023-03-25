1d1e75013987d02b8ba86f257bdf290d5173e32a
Sami Ahmed <unixsam@gmail.com>
2023-03-25 16:38:40 +0400

    Update view for mobile use

--- index.js
+++ index.js
@@ -90,7 +90,7 @@ app.get('/last-added', (req, res) => {
 });
 
 
-app.get('/last-added-date', async (req, res) => {
+app.get('/last-added-10', async (req, res) => {
     try {
       const [rows, _] = await pool.execute('SELECT * FROM sensor_data ORDER BY created_at DESC LIMIT 10');
       res.render('last_added_data', { data: rows });
